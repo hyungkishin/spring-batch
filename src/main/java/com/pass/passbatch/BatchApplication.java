e66320fcd2ce5b7c5c8f20f0e8e343104db48ee6
신형기 <tkaqkeldk@hanatour.com>
2022-12-24 21:00:48 +0900

    [init] initial Commit

---
+++ src/main/java/com/pass/passbatch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.pass.passbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+}
